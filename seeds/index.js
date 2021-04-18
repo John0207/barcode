7db69241c827e391397847a20a7f4816656796c5
John0207 <john.deluccia@snhu.edu>
2021-04-18 00:51:04 -0400

    seed db

---
+++ seeds/index.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const Item = require('../models/item');
+const items = require('./items');
+
+mongoose.connect('mongodb://localhost:27017/barcode', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", () => {
+    console.log('database connected');
+});
+
+const seedDB = async () => {
+    await Item.deleteMany({});
+    for (let i = 0; i < items.length; i++) {
+        const item = new Item({
+            title: `${items[i].title}`,
+            upc: `${items[i].upc}`,    
+            quantity: `${items[i].quantity}`,    
+            price: `${items[i].price}`,   
+            
+        })
+        await item.save();
+    }
+}
+
+seedDB().then(() => {
+    mongoose.connection.close();
+})
+
+
---
+++ seeds/items.js
@@ -0,0 +1,32 @@
+module.exports = [
+    {
+        title: 'item 1', 
+        upc: 681131247634,
+        quantity: 1,
+        price: 20,        
+    },        
+    {
+        title: 'item 2', 
+        upc: 681131087483,
+        quantity: 12,
+        price: 15,        
+    },        
+    {
+        title: 'item 3', 
+        upc: 681131158121,
+        quantity: 13,
+        price: 8,        
+    },        
+    {
+        title: 'item 4', 
+        upc: 810011753013,
+        quantity: 2,
+        price: 5,        
+    },        
+    {
+        title: 'item 5', 
+        upc: 681131247634,
+        quantity: 7,
+        price: 2,        
+    },        
+];
